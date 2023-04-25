9446dd00c3762b99d1fba185f6bfa1c75546cdd3
Sajal Shrestha <sajal@Sajals-MacBook-Pro.local>
2023-04-24 22:36:40 -0700

    feat: update study datas

--- src/Pages/StudyData/Studies/Study/Study.tsx
+++ src/Pages/StudyData/Studies/Study/Study.tsx
@@ -1,98 +1,121 @@
 import React from "react";
 import axios from "axios";
-
+import "./Study.css";
 import { useParams } from "react-router-dom";
-import { Container, Box, Button, Typography, Grid, Modal, IconButton } from "@mui/material";
-import { alpha, styled } from '@mui/material/styles';
-import Table from '@mui/material/Table';
-import TableBody from '@mui/material/TableBody';
-import TableCell, { tableCellClasses } from '@mui/material/TableCell';
-import TableContainer from '@mui/material/TableContainer';
-import TableHead from '@mui/material/TableHead';
-import TableRow from '@mui/material/TableRow';
-import Paper from '@mui/material/Paper';
-import { DateTime } from "luxon";
+import {
+  Container,
+  Box,
+  Typography,
+  Grid,
+  IconButton,
+} from "@mui/material";
 import { useNavigate } from "react-router-dom";
 
 import ResponsiveAppBar from "../../../../Components/AppBar/AppBar";
-import UserData from "../../UserData/UserData";
-import DownloadIcon from '@mui/icons-material/Download';
-import { CSVLink, CSVDownload } from "react-csv";
-
-const StyledTableCell = styled(TableCell)(({ theme }) => ({
-    [`&.${tableCellClasses.head}`]: {
-      backgroundColor: theme.palette.common.white,
-      color: theme.palette.common.black,
-    },
-    [`&.${tableCellClasses.body}`]: {
-      fontSize: 14,
-    },
-  }));
-  
-  const StyledTableRow = styled(TableRow)(({ theme }) => ({
-    '&:nth-of-type(odd)': {
-      backgroundColor: theme.palette.action.hover,
-    },
-    // hide last border
-    '&:last-child td, &:last-child th': {
-      border: 0,
-    },
-  }));
+import DownloadIcon from "@mui/icons-material/Download";
+import Divider from "@mui/material/Divider";
+import RemoveRedEyeIcon from "@mui/icons-material/RemoveRedEye";
+import CsvDownloadButton from "react-json-to-csv";
 
 const Study = () => {
   const { studyDataId, studyDataName } = useParams();
   const [studyData, setStudyData] = React.useState([]);
-  const [selectedUserData, setSelectedUserData] = React.useState({});
   const navigate = useNavigate();
   React.useEffect(() => {
     axios
-      .get(`${process.env.REACT_APP_API_URL}api/getUserGameData/studyId/${studyDataId}`)
+      .get(
+        `${process.env.REACT_APP_API_URL}api/getUserGameData/studyId/${studyDataId}`
+      )
       .then((response) => {
         setStudyData(response.data);
       });
   }, []);
-  const onRowClick = (userGameData:any) => {
-    setSelectedUserData(userGameData)
+  const onRowClick = (userGameData: any) => {
     navigate(`./userGameData/${userGameData._id}`);
   };
-  const downloadCSV = () => {
-    console.log('download!')
-  };
+
   return (
-    <Box>
+    <Box mb={6}>
       <ResponsiveAppBar />
       {studyData ? (
-          <Container>
-           <Box mt={2}>
-            <Box mb={2}><Typography fontSize={30}> {studyDataName}:</Typography></Box>
-           <TableContainer component={Paper}>
-             <Table sx={{ minWidth: 700 }} aria-label="customized table">
-               <TableHead>
-                 <TableRow>
-                  <StyledTableCell>SN. </StyledTableCell>
-                   <StyledTableCell>Alias name</StyledTableCell>
-                   <StyledTableCell align="center">Created At</StyledTableCell>
-                   {/* <StyledTableCell align="center">Download CSV</StyledTableCell> */}
-                 </TableRow>
-               </TableHead>
-               <TableBody>
-                 {studyData.map((study:any, index: any) => (
-                   <StyledTableRow key={study.name} hover={true} onClick={() => onRowClick(study)}>
-                    <StyledTableCell component="th" scope="row">
-                       {index + 1}.
-                     </StyledTableCell>
-                     <StyledTableCell component="th" scope="row">
-                       {study.name}
-                     </StyledTableCell>
-                     <StyledTableCell align="center">{DateTime.fromISO(study.createdAt).toLocaleString(DateTime.DATETIME_MED)}</StyledTableCell>
-                     {/* <StyledTableCell align="center"><IconButton aria-label="download" onClick={downloadCSV}><DownloadIcon/></IconButton></StyledTableCell> */}
-                   </StyledTableRow>
-                 ))}
-               </TableBody>
-             </Table>
-           </TableContainer>
-           </Box>
-           </Container>
+        <Container>
+          <Box mt={2}>
+            <Box mb={2}>
+              <Typography fontSize={30}> {studyDataName}:</Typography>
+            </Box>
+            <Grid container>
+              <Grid item xs={2}>
+                <Typography ml={2}>SN. </Typography>
+              </Grid>
+              <Grid item xs={2}>
+                <Typography ml={4}>Alias Name</Typography>
+              </Grid>
+              <Grid item xs={2}>
+                <Typography ml={2}>Group</Typography>
+              </Grid>
+              <Grid item xs={2}>
+                <Typography ml={1}>View Data</Typography>
+              </Grid>
+              <Grid item xs={2}>
+                <Typography ml={2}>Download</Typography>
+              </Grid>
+            </Grid>
+            <Divider />
+            <Box>
+              {studyData.map((study: any, index: any) => (
+                <Grid container className="data" ml={2} mt={1}>
+                  <Grid item xs={2}>
+                    <Typography ml={1} mt={1}>
+                      {index + 1}.{" "}
+                    </Typography>
+                  </Grid>
+                  <Grid item xs={2}>
+                    <Typography ml={2} mt={1}>
+                      {study.name}
+                    </Typography>
+                  </Grid>
+                  <Grid item xs={2}>
+                    <Typography ml={2} mt={1}>
+                      {study.group}
+                    </Typography>
+                  </Grid>
+                  <Grid item xs={2}>
+                    <Typography ml={1}>
+                      <IconButton
+                        aria-label="view data"
+                        onClick={() => onRowClick(study)}
+                      >
+                        <RemoveRedEyeIcon />
+                      </IconButton>
+                    </Typography>
+                  </Grid>
+                  <Grid item xs={2}>
+                    <Typography ml={2}>
+                      <IconButton aria-label="download data">
+                        <CsvDownloadButton
+                          data={study.records.map((rec: any)=> {
+                            return {
+                              Time: (rec.secondsFromStart - study.records[0].secondsFromStart).toFixed(4),
+                              Event: rec.eventName,
+                              EventModifiers: rec.eventModifiers
+                            }
+                          })}
+                          filename={`${study.name}.csv`}
+                          style={{
+                            background: "none",
+                            border: "none",
+                          }}
+                        >
+                          <DownloadIcon />
+                        </CsvDownloadButton>
+                      </IconButton>
+                    </Typography>
+                  </Grid>
+                </Grid>
+              ))}
+            </Box>
+          </Box>
+        </Container>
       ) : (
         <Box>Loading...</Box>
       )}
